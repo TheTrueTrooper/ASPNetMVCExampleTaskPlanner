e55765cb87d1a86795310edd7d506c6210ff1c04
Angelo Sanches <asanches2@studentmail.nait.ca>
2017-04-07 09:26:54 -0600

    Init Upload made a quick dashboard to start
    Using ASP.NET MVC with razor I am creating a task manager site to show
    case my Model View Control skills and build on them.

---
+++ ASP.NetMVCExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NetMVCExample.Startup))]
+namespace ASP.NetMVCExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
