116cde225eecd903d73f595aa1ee2fdf5df89963
Angelo Sanches <asanches2@studentmail.nait.ca>
2017-06-07 19:49:11 -0600

    Did some things Like SMPTClient
    SMPTClient in SMPTHelper
    -Makes a client to send Emails
    -Then can send a Email
    ->May make a templating engni of some kind or adapt the razer if posible
    Started a TypeScript module for Doing The Gant Drawing (It's kind of a
    ruff first look)
    ->Module Has Class Graph, Gant, Linker, and vector
    ->Graph is a kind of virtual class for Gant to build off of. Has a
    uncoventional virtual function for rendering to allow different graphs
    to be Drawn and some basic graphics starters stuff.
    ->Gant Is an extention of Graph will take the class and build on it the
    tasks extra
    ->Task is a class to hold the task data for sql. May need to extend this
    too so we can draw it diferent ways. Im still exploring posibles in my
    head.
    ->Linker is the same thing. It will how ever compress links from the sql
    into one the way ime going.

--- ASP.NetMVCExample/Scripts/TypeScriptOutput/Gant.js
+++ ASP.NetMVCExample/Scripts/TypeScriptOutput/Gant.js
@@ -1,13 +1,94 @@
 "use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 /// <reference path="../typedeffs/moment.d.ts" />
 var MoDate = require("../typedeffs/moment.d.ts");
-var Task = (function () {
-    function Task(name, StartDate, EndDate) {
-        this.Name = name;
-        this.StartDate = StartDate;
-        this.EndDate = EndDate;
-        this.TimeLength = MoDate.duration(EndDate.diff(StartDate));
-    }
-    return Task;
-}());
+var GantChart;
+(function (GantChart_1) {
+    var Vector2D = (function () {
+        function Vector2D(X, Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+        Vector2D.prototype.Add = function (Vector) {
+            this.X += Vector.X;
+            this.Y += Vector.Y;
+        };
+        Vector2D.prototype.Sub = function (Vector) {
+            this.X -= Vector.X;
+            this.Y -= Vector.Y;
+        };
+        Vector2D.prototype.Scale = function (Number) {
+            this.X *= Number;
+            this.Y *= Number;
+        };
+        Vector2D.prototype.ToString = function () {
+            return this.X.toString() + "," + this.Y.toString() + " ";
+        };
+        return Vector2D;
+    }());
+    var Graph = (function () {
+        function Graph(Canvas) {
+            // to overload it should be set to OverloadRender with a void typodis
+            // it should do the main render you could also draw axis here
+            // wish I could type define this more but you cant function type or else I dont know how :'0 really should have one if you want to extend
+            this._VirtalRender = null;
+            this._Canvas = Canvas;
+            this._Size = new Vector2D(Canvas.getBoundingClientRect().width, Canvas.getBoundingClientRect().height);
+            this._StartPoint = new Vector2D(0, 0);
+            //rest per graph after
+            this._UnitPxScale = new Vector2D(50, 50);
+        }
+        Graph.prototype.Render = function () {
+            if (this._VirtalRender != null)
+                this._VirtalRender();
+        };
+        return Graph;
+    }());
+    var GantChart = (function (_super) {
+        __extends(GantChart, _super);
+        function GantChart(Canvas) {
+            _super.call(this, Canvas);
+            this._AxisTickScale = new Vector2D(5, 1);
+            this._UnitPxScale = new Vector2D(50, 50);
+            this._VirtalRender = this.OverloadRender;
+        }
+        GantChart.prototype.OverloadRender = function () {
+            var _this = this;
+            this._Chains.forEach(function (x) {
+                x.Render(_this._Canvas);
+            });
+        };
+        return GantChart;
+    }(Graph));
+    var Task = (function () {
+        function Task(name, StartDate, EndDate) {
+            this._TaskName = name;
+            this._StartDate = StartDate;
+            this._EndDate = EndDate;
+            this._TimeLength = MoDate.duration(EndDate.diff(StartDate));
+        }
+        Task.prototype.ConstructFromDuration = function (name, StartDate, Duration) {
+            return new Task(name, StartDate, StartDate.add(Duration));
+        };
+        Task.prototype.Render = function (Canvas) {
+        };
+        return Task;
+    }());
+    var Linker = (function () {
+        function Linker(StartTask, EndTask) {
+            this._StartTask = StartTask;
+            this._EndTask = EndTask;
+        }
+        Linker.prototype.Render = function (Canvas) {
+            this._StartTask.forEach(function (x) {
+                x.Render(Canvas);
+            });
+        };
+        return Linker;
+    }());
+})(GantChart || (GantChart = {}));
 //# sourceMappingURL=Gant.js.map
--- ASP.NetMVCExample/src/Code/Gant.ts
+++ ASP.NetMVCExample/src/Code/Gant.ts
@@ -1,23 +1,147 @@
 ﻿/// <reference path="../typedeffs/moment.d.ts" />
 import * as MoDate from "../typedeffs/moment.d.ts";
 
-
-class Task
+module GantChart
 {
-    public Name: string;
-    public StartDate: MoDate.Moment;
-    public EndDate: MoDate.Moment;
-    public TimeLength: MoDate.Duration; 
-
-    constructor(name: string, StartDate: MoDate.Moment, EndDate: MoDate.Moment)
+    class Vector2D
     {
-        this.Name = name;
-        this.StartDate = StartDate;
-        this.EndDate = EndDate;
-        this.TimeLength = MoDate.duration(EndDate.diff(StartDate));
+        //A simple X cord
+        public X: number;
+        //A simple Y cord
+        public Y: number;
+
+        constructor(X: number, Y: number)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public Add(Vector: Vector2D)
+        {
+            this.X += Vector.X;
+            this.Y += Vector.Y;
+        }
+
+        public Sub(Vector: Vector2D)
+        {
+            this.X -= Vector.X;
+            this.Y -= Vector.Y;
+        }
+
+        public Scale(Number: number)
+        {
+            this.X *= Number;
+            this.Y *= Number;
+        }
+
+        public ToString(): string
+        {
+            return this.X.toString() + "," + this.Y.toString() + " ";
+        }
     }
 
+    class Graph
+    {
+        //the SVG element to draw on as a Canvas
+        protected _Canvas: SVGSVGElement;
+        // so that we can change the unit value for X, Y (days, years, etc.)
+        protected _UnitPxScale: Vector2D;
+        // so that we can scroll maybe a start point to shift things by
+        protected _StartPoint: Vector2D;
+        // the size of view box we are rendering into
+        protected _Size: Vector2D;
+        // to overload it should be set to OverloadRender with a void typodis
+        // it should do the main render you could also draw axis here
+        // wish I could type define this more but you cant function type or else I dont know how :'0 really should have one if you want to extend
+        protected _VirtalRender = null;
 
-    
+        constructor(Canvas: SVGSVGElement)
+        {
+            this._Canvas = Canvas;
+            this._Size = new Vector2D(Canvas.getBoundingClientRect().width, Canvas.getBoundingClientRect().height);
+            this._StartPoint = new Vector2D(0, 0);
+            //rest per graph after
+            this._UnitPxScale = new Vector2D(50, 50);
+        }
+
+        public Render(): void
+        {
+            if (this._VirtalRender != null)
+                this._VirtalRender();
+        }
+    }
+
+    class GantChart extends Graph
+    {
+        // This Is used with the unit scale
+        public _AxisTickScale: Vector2D;
+
+        public _Chains: Linker[];
+
+        constructor(Canvas: SVGSVGElement)
+        {
+            super(Canvas);
+            this._AxisTickScale = new Vector2D(5, 1);
+            this._UnitPxScale = new Vector2D(50, 50);
+            this._VirtalRender = this.OverloadRender;
+        }
+
+        public OverloadRender(): void
+        {
+            this._Chains.forEach(x =>
+            {
+                x.Render(this._Canvas);
+                
+            })
+        }
+    }
+
+    class Task
+    {
+        private _TaskName: string;
+        private _StartDate: MoDate.Moment;
+        private _EndDate: MoDate.Moment;
+        private _TimeLength: MoDate.Duration;
+
+        constructor(name: string, StartDate: MoDate.Moment, EndDate: MoDate.Moment)
+        {
+            this._TaskName = name;
+            this._StartDate = StartDate;
+            this._EndDate = EndDate;
+            this._TimeLength = MoDate.duration(EndDate.diff(StartDate));
+        }
+
+        public ConstructFromDuration(name: string, StartDate: MoDate.Moment, Duration: MoDate.Duration): Task
+        {
+            return new Task(name, StartDate, StartDate.add(Duration));
+        }
+
+        public Render(Canvas: SVGSVGElement): void
+        {
+            
+
+        }
+
+    }
+
+    class Linker
+    {
+        private _StartTask: Task[];
+        private _EndTask: Task[];
+        
+        constructor(StartTask: Task[], EndTask: Task[])
+        {
+            this._StartTask = StartTask;
+            this._EndTask = EndTask; 
+        }
+
+        public Render(Canvas: SVGSVGElement): void
+        {
+            this._StartTask.forEach(x =>
+            {
+                x.Render(Canvas);
+            });
+        }
+    }
 
 }
